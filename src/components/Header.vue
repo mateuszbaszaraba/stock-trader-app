651db610402e421baaac9702d2a999059bf56749
Matt <mateuszbaszaraba@outlook.com>
2020-05-04 13:45:40 +0200

    added vue-router

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <ul>
+            <router-link tag="li" to="/">Home</router-link>
+            <router-link tag="li" to="/Portfolio">Portfolio</router-link>
+            <router-link tag="li" to="/Stocks">Stocks</router-link>
+        </ul>
+    </div>
+</template>
